cfc1fa8a552314b9a54af3affb97633173dbb5a3
Fabrizio Tettoni <fabrizio@teasistemi.it>
2017-05-03 15:19:44 +0200

    minor changes

--- examples/testshapes_16xMultiple/testshapes_16xMultiple.ino
+++ examples/testshapes_16xMultiple/testshapes_16xMultiple.ino
@@ -21,82 +21,82 @@ RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false, 32*n);
 
 void setup() {
 
-  matrix.begin();
-  
-  // draw a pixel in solid white
-  matrix.drawPixel(16, 0, matrix.Color333(7, 7, 7)); 
-  delay(250);
-
-  // fix the screen with green
-  matrix.fillRect(16, 0, 32, 16, matrix.Color333(0, 7, 0));
-  delay(250);
-
-  // draw a box in yellow
-  matrix.drawRect(16, 0, 32, 16, matrix.Color333(7, 7, 0));
-  delay(250);
-  
-  // draw an 'X' in red
-  matrix.drawLine(16, 0, 47, 15, matrix.Color333(7, 0, 0));
-  matrix.drawLine(47, 0, 16, 15, matrix.Color333(7, 0, 0));
-  delay(250);
-  
-  // draw a blue circle
-  matrix.drawCircle(23, 7, 7, matrix.Color333(0, 0, 7));
-  delay(250);
-  
-  // fill a violet circle
-  matrix.fillCircle(39, 7, 7, matrix.Color333(7, 0, 7));
-  delay(250);
-  
-  // fill the screen with 'black'
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-
-  // draw a box in Green
-  matrix.fillRect(0, 0, 16, 16, matrix.Color333(0, 7, 0));
-  delay(250);
-
-  // draw a box in white
-  matrix.fillRect(16, 0, 32, 16, matrix.Color333(7, 7, 7));
-  delay(250);
-
-  // draw a box in Red
-  matrix.fillRect(48, 0, 16, 16, matrix.Color333(7, 0, 0));
-  delay(250);
-  
-  // draw some text!
-  matrix.setCursor(17, 0);   // start at top left, with one pixel of spacing
-  matrix.setTextSize(1);    // size 1 == 8 pixels high
-
-  // fill the screen with 'black'
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-  
-  // print each letter with a rainbow color
-  matrix.setTextColor(matrix.Color333(7,0,0));
-  matrix.print('1');
-  matrix.setTextColor(matrix.Color333(7,4,0)); 
-  matrix.print('6');
-  matrix.setTextColor(matrix.Color333(7,7,0));
-  matrix.print('x');
-  matrix.setTextColor(matrix.Color333(4,7,0)); 
-  matrix.print('6');
-  matrix.setTextColor(matrix.Color333(0,7,0));  
-  matrix.print('4');
-  
-  matrix.setCursor(17, 9);   // next line
-  matrix.setTextColor(matrix.Color333(0,7,7)); 
-  matrix.print('*');
-  matrix.setTextColor(matrix.Color333(0,4,7)); 
-  matrix.print('R');
-  matrix.setTextColor(matrix.Color333(0,0,7));
-  matrix.print('G');
-  matrix.setTextColor(matrix.Color333(4,0,7)); 
-  matrix.print('B');
-  matrix.setTextColor(matrix.Color333(7,0,4)); 
-  matrix.print('*');
-
-  // whew!
+    matrix.begin();
+    
+    // draw a pixel in solid white
+    matrix.drawPixel(16, 0, matrix.Color333(7, 7, 7)); 
+    delay(250);
+    
+    // fix the screen with green
+    matrix.fillRect(16, 0, 32, 16, matrix.Color333(0, 7, 0));
+    delay(250);
+    
+    // draw a box in yellow
+    matrix.drawRect(16, 0, 32, 16, matrix.Color333(7, 7, 0));
+    delay(250);
+    
+    // draw an 'X' in red
+    matrix.drawLine(16, 0, 47, 15, matrix.Color333(7, 0, 0));
+    matrix.drawLine(47, 0, 16, 15, matrix.Color333(7, 0, 0));
+    delay(250);
+    
+    // draw a blue circle
+    matrix.drawCircle(23, 7, 7, matrix.Color333(0, 0, 7));
+    delay(250);
+    
+    // fill a violet circle
+    matrix.fillCircle(39, 7, 7, matrix.Color333(7, 0, 7));
+    delay(250);
+    
+    // fill the screen with 'black'
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    
+    // draw a box in Green
+    matrix.fillRect(0, 0, 16, 16, matrix.Color333(0, 7, 0));
+    delay(250);
+    
+    // draw a box in white
+    matrix.fillRect(16, 0, 32, 16, matrix.Color333(7, 7, 7));
+    delay(250);
+    
+    // draw a box in Red
+    matrix.fillRect(48, 0, 16, 16, matrix.Color333(7, 0, 0));
+    delay(250);
+    
+    // draw some text!
+    matrix.setCursor(17, 0);   // start at top left, with one pixel of spacing
+    matrix.setTextSize(1);    // size 1 == 8 pixels high
+    
+    // fill the screen with 'black'
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    
+    // print each letter with a rainbow color
+    matrix.setTextColor(matrix.Color333(7,0,0));
+    matrix.print('1');
+    matrix.setTextColor(matrix.Color333(7,4,0)); 
+    matrix.print('6');
+    matrix.setTextColor(matrix.Color333(7,7,0));
+    matrix.print('x');
+    matrix.setTextColor(matrix.Color333(4,7,0)); 
+    matrix.print('6');
+    matrix.setTextColor(matrix.Color333(0,7,0));  
+    matrix.print('4');
+    
+    matrix.setCursor(17, 9);   // next line
+    matrix.setTextColor(matrix.Color333(0,7,7)); 
+    matrix.print('*');
+    matrix.setTextColor(matrix.Color333(0,4,7)); 
+    matrix.print('R');
+    matrix.setTextColor(matrix.Color333(0,0,7));
+    matrix.print('G');
+    matrix.setTextColor(matrix.Color333(4,0,7)); 
+    matrix.print('B');
+    matrix.setTextColor(matrix.Color333(7,0,4)); 
+    matrix.print('*');
+    
+    // whew!
 }
 
 void loop() {
-  // do nothing
+    // do nothing
 }
