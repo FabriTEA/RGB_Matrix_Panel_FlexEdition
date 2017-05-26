13f27cd6bd0b4580663e2d1c4b1d237e8693406f
Fabrizio Tettoni <fabrizio@teasistemi.it>
2017-05-27 00:38:52 +0200

    Update RGBmatrixPanel.h

--- RGBmatrixPanel.h
+++ RGBmatrixPanel.h
@@ -29,6 +29,7 @@ class RGBmatrixPanel : public Adafruit_GFX {
   void
     begin(void),
     drawPixel(int16_t x, int16_t y, uint16_t c),
+    setCursor(uint8_t x, uint8_t y),
     fillScreen(uint16_t c),
     updateDisplay(void),
     swapBuffers(boolean),
